eebc4fe8995e7250da5054f76feec18c7edcb78d
Michael Decroce-Movson <mdecrocemovson@gmail.com>
2020-04-07 22:40:40 -0400

    formik

--- src/components/AddInvestor.js
+++ src/components/AddInvestor.js
@@ -3,6 +3,18 @@ import { Form, Button } from "react-bootstrap";
 import "../styles/AddInvestor.scss";
 import SelectUSState from "react-select-us-states";
 import FileBase64 from "react-file-base64";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const schema = Yup.object({
+  firstName: Yup.string().required(),
+  lastName: Yup.string().required(),
+  dateOfBirth: Yup.string().required(),
+  phoneNumber: Yup.string().required(),
+  state: Yup.string().required(),
+  zip: Yup.string().required(),
+  terms: Yup.bool().required(),
+});
 
 const AddInvestor = () => {
   const [firstName, setFirstName] = useState("");
@@ -37,102 +49,119 @@ const AddInvestor = () => {
 
   const selectImage = (files) => {
     setFile(files[0].base64);
-  }
+  };
   return (
     <div className="AddInvestor-formContainer">
-      <div className="AddInvestor-formElements">
-        <Form>
-          <h1>Get Accredited!</h1>
-          <Form.Group controlId="formFirstName">
-            <Form.Label>Investor First Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter first name"
-              onChange={(event) => setFirstName(event.target.value)}
-            />
-          </Form.Group>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+        }}
+        validationSchema={schema}
+        onSubmit={(values) => {
+          // same shape as initial values
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div className="AddInvestor-formElements">
+              <h1>Get Accredited!</h1>
+              <Form.Group controlId="formFirstName">
+                <Form.Label>Investor First Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter first name"
+                  onChange={(event) => setFirstName(event.target.value)}
+                />
+                {errors.firstName && touched.firstName ? (
+                  <div>{errors.firstName}</div>
+                ) : null}
+              </Form.Group>
 
-          <Form.Group controlId="formLastName">
-            <Form.Label>Investor Last Name</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter last name"
-              onChange={(event) => setLastName(event.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="formDateOfBirth">
-            <Form.Label>Date of birth</Form.Label>
-            <Form.Control
-              type="date"
-              placeholder="Enter date of birth"
-              onChange={(event) => setDateOfBirth(event.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="formPhoneNumber">
-            <Form.Label>Investor Phone Number</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter phone number"
-              onChange={(event) => setPhoneNumber(event.target.value)}
-            />
-          </Form.Group>
-          <div className="AddInvestor-location">
-            <Form.Group
-              className="AddInvestor-locationInput"
-              controlId="formStreetAddress"
-            >
-              <Form.Label>Investor Street Address</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter Street Address"
-                onChange={(event) => setStreetAddress(event.target.value)}
-              />
-            </Form.Group>
-            <div className="AddInvestor-statesContainer">
-              <Form.Group
-                className="AddInvestor-locationInput"
-                controlId="formStreetAddress"
-              >
-                <Form.Label>Us States</Form.Label>
-                <SelectUSState
-                  className="AddInvestor-states"
-                  onChange={(value) => setState(value)}
+              <Form.Group controlId="formLastName">
+                <Form.Label>Investor Last Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter last name"
+                  onChange={(event) => setLastName(event.target.value)}
                 />
               </Form.Group>
+              <Form.Group controlId="formDateOfBirth">
+                <Form.Label>Date of birth</Form.Label>
+                <Form.Control
+                  type="date"
+                  placeholder="Enter date of birth"
+                  onChange={(event) => setDateOfBirth(event.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="formPhoneNumber">
+                <Form.Label>Investor Phone Number</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Enter phone number"
+                  onChange={(event) => setPhoneNumber(event.target.value)}
+                />
+              </Form.Group>
+              <div className="AddInvestor-location">
+                <Form.Group
+                  className="AddInvestor-locationInput"
+                  controlId="formStreetAddress"
+                >
+                  <Form.Label>Investor Street Address</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Street Address"
+                    onChange={(event) => setStreetAddress(event.target.value)}
+                  />
+                </Form.Group>
+                <div className="AddInvestor-statesContainer">
+                  <Form.Group
+                    className="AddInvestor-locationInput"
+                    controlId="formStreetAddress"
+                  >
+                    <Form.Label>Us States</Form.Label>
+                    <SelectUSState
+                      className="AddInvestor-states"
+                      onChange={(value) => setState(value)}
+                    />
+                  </Form.Group>
+                </div>
+                <Form.Group
+                  className="AddInvestor-locationInput"
+                  controlId="formZip"
+                >
+                  <Form.Label>Investor Zip Code</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter zip"
+                    onChange={(event) => setZipCode(event.target.value)}
+                  />
+                </Form.Group>
+              </div>
+              <FileBase64 multiple={true} onDone={selectImage} />
+              <button
+                onClick={() =>
+                  handleSubmit({
+                    investor: {
+                      firstName,
+                      lastName,
+                      dateOfBirth,
+                      phoneNumber,
+                      streetAddress,
+                      state,
+                      zipCode,
+                      file,
+                    },
+                  })
+                }
+              >
+                Submit
+              </button>
             </div>
-            <Form.Group
-              className="AddInvestor-locationInput"
-              controlId="formZip"
-            >
-              <Form.Label>Investor Zip Code</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter zip"
-                onChange={(event) => setZipCode(event.target.value)}
-              />
-            </Form.Group>
-          </div>
-          <FileBase64 multiple={true} onDone={selectImage} />
-        </Form>
-        <button
-          onClick={() =>
-            handleSubmit({
-              investor: {
-                firstName,
-                lastName,
-                dateOfBirth,
-                phoneNumber,
-                streetAddress,
-                state,
-                zipCode,
-                file,
-              },
-            })
-          }
-        >
-          Submit
-        </button>
-      </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
